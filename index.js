ce64554f3eb1ee02d6c112bb7f28d0b5a63e7ae5
Ville_ThinkPad <ville.ponnelin@aalto.fi>
2021-12-18 16:52:18 +0200

    Add frontend deployment (3.10)

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 morgan.token('body', (req, res) => {
